87e190964860596df5d4b207a16c5aa782037c29
Alexey <alexey_kuzminykh@mail.ru>
2023-01-20 13:38:22 +0300

    Напишите пример перехвата и обработки исключения с использованием собственных исключений.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ScannerException {
+        //Напишите пример перехвата и обработки исключения с использованием собственных исключений.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int x = Integer.parseInt(scanner.nextLine());
+
+            if (x == 0 ){
+                throw new ScannerException("Все кроме 0");
+
+            }
+        }
+
     }
 }
